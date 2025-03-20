aa21397cd22c6bd3522e29e30fd0c37d3af1725e
RezaAzharii <rezaazhari867@gmail.com>
2025-03-20 22:19:56 +0700

    Menambahkan input dedline dengan layouting row dan colum

--- lib/todo_app.dart
+++ lib/todo_app.dart
@@ -9,7 +9,7 @@ class TodoApp extends StatefulWidget {
 
 class _TodoAppState extends State<TodoApp> {
   final GlobalKey<FormState> key = GlobalKey<FormState>();
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +28,47 @@ class _TodoAppState extends State<TodoApp> {
         padding: EdgeInsets.all(18),
         child: Column(
           children: [
+            Form(
+              key: key,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Deadline : ",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            "Pilih Deadline",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey[200],
+                            ),
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        onPressed:() {},
+                        icon: Icon(
+                          Icons.date_range,
+                          color: Colors.pinkAccent,
+                          size: 30,
+                        ),
+                      ),
+                    ],
+                  ),
 
+                ],
+              )
+            )
           ],
         ),
       ),
